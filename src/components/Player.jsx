71901cb0dd7bdbb6b3ff235c69a2f2fff1e8dfcc
Oleksiichuk Mykyta <mykyta.oleks.dev@gmail.com>
2025-08-12 16:11:52 +0300

    feat: connected input and button to welcoming header with ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,21 @@
+import { useRef, useState } from 'react';
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	const inputRef = useRef(null);
+	const [name, setName] = useState('');
+
+	const handleClick = () => {
+		setName(inputRef.current.value);
+    inputRef.current.value = '';
+	};
+
+	return (
+		<section id="player">
+			<h2>Welcome {name || 'unknown entity'}</h2>
+			<p>
+				<input ref={inputRef} type="text" />
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	);
 }
